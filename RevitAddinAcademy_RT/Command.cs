4310114a071af97df90e610148d517bfdb633996
rtandy <rebeccat@alliancearch.com>
2022-07-06 16:41:14 -0500

    Add classwork for Session 02

--- RevitAddinAcademy_RT/Command.cs
+++ RevitAddinAcademy_RT/Command.cs
@@ -7,6 +7,7 @@ using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
 
 #endregion
 
@@ -25,32 +26,60 @@ namespace RevitAddinAcademy_RT
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            // Access current selection
+            string excelFile = @"C:\Users\rebeccat\Documents\_Revit Training\Revit Add-in Academy\Session 02\Sheets.xlsx";
 
-            Selection sel = uidoc.Selection;
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook excelWb = excelApp.Workbooks.Open(excelFile);
+            //excel starts numbering at 1, not 0
+            Excel.Worksheet excelWs = excelWb.Worksheets.Item[1];
 
-            // Retrieve elements from database
+            Excel.Range excelRng = excelWs.UsedRange;
+            int rowCount = excelRng.Rows.Count;
 
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
-
-            // Filtered element collector is iterable
-
-            foreach (Element e in col)
+            //do some stuff in excel
+            List<string[]> dataList = new List<string[]>();
+            for (int i = 1; i < rowCount; i++)
             {
-                Debug.Print(e.Name);
+                Excel.Range cell1 = excelWs.Cells[i, 1];
+                Excel.Range cell2 = excelWs.Cells[i, 2];
+
+                string data1 = cell1.Value.ToString();
+                string data2 = cell2.Value.ToString();
+
+                string[] dataArray = new string[2];
+                dataArray[0] = data1;
+                dataArray[1] = data2;
+
+                dataList.Add(dataArray);
+
+
             }
 
-            // Modify document within a transaction
-
-            using (Transaction tx = new Transaction(doc))
+            using(Transaction t = new Transaction(doc))
             {
-                tx.Start("Transaction Name");
-                tx.Commit();
+                t.Start("Create some Revit stuff");
+                
+                //create level
+                Level curLevel = Level.Create(doc, 100);
+
+
+                 //create sheet
+
+                FilteredElementCollector collector = new FilteredElementCollector(doc);
+                collector.OfCategory(BuiltInCategory.OST_TitleBlocks);
+                //specify types, not instances
+                collector.WhereElementIsElementType();
+
+                ViewSheet curSheet = ViewSheet.Create(doc, collector.FirstElementId());
+                curSheet.SheetNumber = "A101";
+                curSheet.Name = "New Sheet";
+
+                t.Commit();
             }
+
+            
+            excelWb.Close();
+            excelApp.Quit();
 
             return Result.Succeeded;
         }
