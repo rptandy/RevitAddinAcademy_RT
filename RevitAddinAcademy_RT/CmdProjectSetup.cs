c8cb4688d62efc97a081b1d83ca0279774732bc3
rtandy <rebeccat@alliancearch.com>
2022-07-15 09:42:08 -0500

    Challenge 03 & Extra Credit - initial commit

--- RevitAddinAcademy_RT/CmdProjectSetup.cs
+++ RevitAddinAcademy_RT/CmdProjectSetup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using Excel = Microsoft.Office.Interop.Excel;
 using Forms = System.Windows.Forms;
+using System.IO;
 
 #endregion
 
@@ -27,61 +28,266 @@ namespace RevitAddinAcademy_RT
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            string excelFile = @"C:\Users\rebeccat\Documents\_Revit Training\Revit Add-in Academy\Session 02\Session02_Challenge.xlsx";
+            //Get Excel file
+            Forms.OpenFileDialog openFileDialog = new Forms.OpenFileDialog();
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "Excel files (*.xls)|*xls* | All files (*.*)|*.*";
 
-            Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook excelWb = excelApp.Workbooks.Open(excelFile);
+            string filePath = "";
 
-            Excel.Worksheet excelWs1 = excelApp.Worksheets.Item[1]; //Levels
-            Excel.Range excelRng1 = excelWs1.UsedRange;
-            int rowCount1 = excelRng1.Rows.Count;
-            
-            Excel.Worksheet excelWs2 = excelApp.Worksheets.Item[2]; //Sheets
-            Excel.Range excelRng2 = excelWs2.UsedRange;
-            int rowCount2 = excelRng2.Rows.Count;
-
-            using (Transaction tx = new Transaction(doc))
+            if (openFileDialog.ShowDialog() == Forms.DialogResult.OK)
             {
-                tx.Start("Transaction Name");
+               filePath = openFileDialog.FileName;
+            }
 
-                //Process levels
-                for(int i = 2; i <= rowCount1; i++)
+            try
+            {
+                //Setup Excel
+                Excel.Application excelApp = new Excel.Application();
+                Excel.Workbook excelWb = excelApp.Workbooks.Open(filePath);
+
+                //Levels Data
+                int index1 = GetExcelWorksheetByName(excelWb, "Levels");
+                Excel.Worksheet excelWs1 = excelApp.Worksheets.Item[index1];
+                Excel.Range excelRng1 = excelWs1.UsedRange;
+                int rowCount1 = excelRng1.Rows.Count;
+
+                List<LevelStruct> levelList = new List<LevelStruct>();
+
+                for (int i = 2; i <= rowCount1; i++)
                 {
                     Excel.Range cell1 = excelWs1.Cells[i, 1];
                     Excel.Range cell2 = excelWs1.Cells[i, 2];
 
-                    string levelName = cell1.Value.ToString();
-                    double levelElev = cell2.Value;
+                    LevelStruct levelStruct = new LevelStruct();
+                    levelStruct.levelName = cell1.Value.ToString();
+                    levelStruct.levelElev = cell2.Value;
 
-                    Level newLevel = Level.Create(doc, levelElev);
-                    newLevel.Name = levelName;
+                    levelList.Add(levelStruct);
                 }
 
-                FilteredElementCollector collector = new FilteredElementCollector(doc);
-                collector.OfCategory(BuiltInCategory.OST_TitleBlocks);
-                collector.WhereElementIsElementType();
+                //Sheet Data
+                int index2 = GetExcelWorksheetByName(excelWb, "Sheets");
+                Excel.Worksheet excelWs2 = excelApp.Worksheets.Item[index2];
+                Excel.Range excelRng2 = excelWs2.UsedRange;
+                int rowCount2 = excelRng2.Rows.Count;
 
-                //Process Sheets
-                for (int j = 2; j <= rowCount2; j++)
+                List<SheetStruct> sheetList = new List<SheetStruct>();
+
+                for (int j = 2; j <= rowCount1; j++)
                 {
                     Excel.Range cell1 = excelWs2.Cells[j, 1];
                     Excel.Range cell2 = excelWs2.Cells[j, 2];
+                    Excel.Range cell3 = excelWs2.Cells[j, 3];
+                    Excel.Range cell4 = excelWs2.Cells[j, 4];
+                    Excel.Range cell5 = excelWs2.Cells[j, 5];
 
-                    string sheetNum = cell1.Value.ToString();
-                    string sheetName = cell2.Value.ToString();
+                    SheetStruct sheetStruct = new SheetStruct();
+                    sheetStruct.sheetNum = cell1.Value.ToString();
+                    sheetStruct.sheetName = cell2.Value.ToString();
+                    sheetStruct.viewName = cell3.Value.ToString();
+                    sheetStruct.drawnBy = cell4.Value.ToString();
+                    sheetStruct.checkedBy = cell5.Value.ToString();
 
-                    ViewSheet newSheet = ViewSheet.Create(doc, collector.FirstElementId());
-                    newSheet.Name = sheetName;
-                    newSheet.SheetNumber = sheetNum;
+                    sheetList.Add(sheetStruct);
                 }
 
-                tx.Commit();
+                //Select ViewFamily types for creating Floor Plan and RCP views
+                FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                collector1.OfClass(typeof(ViewFamilyType));
+
+                ViewFamilyType curVFT = null;
+                ViewFamilyType CurRCPVFT = null;
+                foreach(ViewFamilyType curElem in collector1)
+                {
+                    if(curElem.ViewFamily == ViewFamily.FloorPlan)
+                    {
+                        curVFT = curElem;
+                    }
+                    else if(curElem.ViewFamily == ViewFamily.CeilingPlan)
+                    {
+                        CurRCPVFT = curElem;
+                    }
+                }
+
+
+                using (Transaction tx = new Transaction(doc))
+                {
+                    tx.Start("Project Setup");
+
+                    //Process levels
+                    for (int i = 0; i < levelList.Count; i++)
+                    {
+                        LevelStruct curLevel = levelList[i];
+
+                        try
+                        {
+                            Level newLevel = Level.Create(doc, curLevel.levelElev);
+                            newLevel.Name = curLevel.levelName;
+
+                            ViewPlan curPlan = ViewPlan.Create(doc, curVFT.Id, newLevel.Id);
+                            curPlan.Name = curLevel.levelName;
+
+                            ViewPlan curRCP = ViewPlan.Create(doc, CurRCPVFT.Id, newLevel.Id);
+                            curRCP.Name = curLevel.levelName + " RCP";
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.Print(ex.Message);
+                        }
+                    }
+
+                    //Process Sheets
+                    for (int j = 0; j < sheetList.Count; j++)
+                    {
+                        SheetStruct curSheet = sheetList[j];
+                        ElementId titleBlock = GetTitleblockByName(doc, "Alliance 30 x 42");
+                        XYZ insPoint = new XYZ(0,0,0);
+                        View viewAdd = GetViewByName(doc, curSheet.viewName);
+
+                        try
+                        {
+                            ViewSheet newSheet = ViewSheet.Create(doc, titleBlock);
+                            newSheet.Name = curSheet.sheetName;
+                            newSheet.SheetNumber = curSheet.sheetNum;
+
+                            SetParameterValueByName(newSheet, "Drawn By", curSheet.drawnBy);
+                            SetParameterValueByName(newSheet, "Checked By", curSheet.checkedBy);
+
+                            if (viewAdd != null)
+                            {
+                                Viewport newVP = Viewport.Create(doc, newSheet.Id, viewAdd.Id, insPoint);
+                            }
+                            else
+                            {
+                                TaskDialog.Show("Error", "View not found.");
+                            }
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.Print(ex.Message);
+                        }
+                    }
+
+                    tx.Commit();
+                }
+
+                excelWb.Close();
+                excelApp.Quit();
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
             }
 
-            excelWb.Close();
-            excelApp.Quit();
 
             return Result.Succeeded;
         }
+
+        internal struct LevelStruct
+        {
+            public string levelName;
+            public double levelElev;
+        }
+
+        internal struct SheetStruct
+        {
+            public string sheetNum;
+            public string sheetName;
+            public string viewName;
+            public string drawnBy;
+            public string checkedBy;
+        }
+
+        internal View GetViewByName(Document doc, string viewName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(View));
+
+            foreach(View curView in collector)
+            {
+                if(curView.Name == viewName)
+                {
+                    return curView;
+                }
+            }
+
+            return null;
+        }
+
+        public int GetExcelWorksheetByName(Excel.Workbook excelWb, string name)
+        {
+            int count = excelWb.Worksheets.Count;
+            int index = 1;
+
+            for (int i = 1; i <= count; i++)
+            {
+                Excel.Worksheet curWs = excelWb.Worksheets[i];
+                if(curWs.Name == name)
+                {
+                    index = curWs.Index;
+                }
+            }
+
+            return index;
+        }
+        
+        public ElementId GetTitleblockByName(Document doc, string name)
+        {
+            FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+            collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
+            collector2.WhereElementIsElementType();
+
+            string curName = "";
+            ElementId elemID = null;
+
+            foreach (Element e in collector2)
+            {
+                curName = e.Name;
+                if(curName == name)
+                {
+                    elemID = e.Id;
+                }
+            }
+            if(elemID == null)
+            {
+                elemID = collector2.FirstElementId();
+            }
+
+            return elemID;
+
+        }
+        
+        public void SetParameterValueByName(Element element, string paramName, string paramValue)
+        {
+            try
+            {
+                foreach (Parameter curParam in element.Parameters)
+                {
+                    if (curParam.Definition.Name == paramName)
+                    {
+                        if(curParam.StorageType is StorageType.String)
+                        {
+                            curParam.Set(paramValue);
+                        }
+                        else if(curParam.StorageType is StorageType.Double || curParam.StorageType is StorageType.Integer)
+                        {
+                            curParam.SetValueString(paramValue);
+                        }
+                        
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+            
+        }
+
+
     }
 }
